5b40f576f606019ca575a485647a3444dec673a8
eliakimshasha <eliakimshasha@gmail.com>
2024-12-12 23:15:27 +0000

    4 commit

--- app/Screens/Blog/page.jsx
+++ app/Screens/Blog/page.jsx
@@ -1,11 +1,12 @@
 'use client';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import Image from 'next/image';
 import img from '@/assets/images/img11.jpg';
-import BlogCount from '@/app/components/Blog/BlogCount';
+// import BlogCount from '@/app/components/Blog/BlogCount';
 
-function Page() {
+// Function to handle the blog page rendering
+function BlogPage() {
   const searchParams = useSearchParams();
   const image = searchParams.get('image') || img;
   const desc = searchParams.get('desc') || '';
@@ -23,50 +24,40 @@ function Page() {
         </div>
         <h1 className="font-bold mb-3 line-clamp-none text-sm">{title}</h1>
         <div className="flex flex-col justify-center items-center">
-          <div className="rounded-sm h-[30vh] overflow-hidden  w-[100%]" style={{boxShadow:'0 0 10px rgba(0, 0, 0, 0.1)'}}>
-            <Image src={image} alt="image" className="h-full w-full " />
+          <div className="rounded-sm h-[30vh] overflow-hidden w-[100%]" style={{ boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
+            <Image src={image} alt="image" className="h-full w-full" />
           </div>
           <div>
             <h1 className="font-bold py-3 text-sm">
               Revolutionary Advancement: Turbocharged Next.js
             </h1>
-            <p className='text-sm'>
-              {desc} A pivotal enhancement in Next.js 14 lies in its
-              turbocharged compiler. The diligent efforts of the Next.js team
-              manifest in a Rust-based compiler that is poised to achieve
-              stabilization imminently. The commitment to accelerating local
-              server startup and expediting code updates underscores the
-              significance of this development.
+            <p className="text-sm">
+              {desc} A pivotal enhancement in Next.js 14 lies in its turbocharged compiler. The diligent efforts of the Next.js team manifest in a Rust-based compiler that is poised to achieve stabilization imminently. The commitment to accelerating local server startup and expediting code updates underscores the significance of this development.
             </p>
             <h1 className="font-bold py-3 text-sm">
               Empowering Developers: Forms and Mutations
             </h1>
-            <p className='text-sm'>
-              Introducing a streamlined approach to managing forms and
-              mutations, Next.js 14 breaks away from the conventional method
-              where developers were compelled to manually create API routes for
-              handling form submissions. Enter Server Actions, empowering
-              developers to articulate functions executed securely on the server
-              and directly callable from React.
+            <p className="text-sm">
+              Introducing a streamlined approach to managing forms and mutations, Next.js 14 breaks away from the conventional method where developers were compelled to manually create API routes for handling form submissions. Enter Server Actions, empowering developers to articulate functions executed securely on the server and directly callable from React.
             </p>
             <h1 className="font-bold py-3 text-sm">
               Revolutionary Advancement: Turbocharged Next.js
             </h1>
-            <p className='text-sm'>
-              {desc} A pivotal enhancement in Next.js 14 lies in its
-              turbocharged compiler. The diligent efforts of the Next.js team
-              manifest in a Rust-based compiler that is poised to achieve
-              stabilization imminently. The commitment to accelerating local
-              server startup and expediting code updates underscores the
-              significance of this development.
+            <p className="text-sm">
+              {desc} A pivotal enhancement in Next.js 14 lies in its turbocharged compiler. The diligent efforts of the Next.js team manifest in a Rust-based compiler that is poised to achieve stabilization imminently. The commitment to accelerating local server startup and expediting code updates underscores the significance of this development.
             </p>
           </div>
         </div>
-      
       </div>
-     <BlogCount/>
+      {/* <BlogCount /> */}
     </div>
   );
 }
 
-export default Page;
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <BlogPage />
+    </Suspense>
+  );
+}
