ebd317ebec1082be1a8b8931aebb57ebd41041eb
eliakimshasha <eliakimshasha@gmail.com>
2024-12-12 22:53:05 +0000

    third commit

--- app/components/json/products.jsx
+++ app/components/json/products.jsx
@@ -2,8 +2,8 @@ import img1 from '@/assets/images/cloves.jpg';
 import img2 from '@/assets/images/on1.jpg';
 import img3 from '@/assets/images/guaca.jpeg';
 import img4 from '@/assets/images/p1.jpg';
-import img5 from '@/assets/images/crude-corn-oilf.jpg';
-import img6 from '@/assets/images/soybean-oil1f.jpg';
+// import img5 from '@/assets/images/crude-corn-oilf.jpg';
+// import img6 from '@/assets/images/soybean-oil1f.jpg';
 
 
 
@@ -14,12 +14,12 @@ export  const products = [
       title: '+ Cloves CG3',
       desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
     },
-    {
-      id: 2,
-      image: img6,
-      title: '+ Cloves CG3',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-    },
+    // {
+    //   id: 2,
+    //   image: img6,
+    //   title: '+ Cloves CG3',
+    //   desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+    // },
 
     {
       id: 3,
@@ -39,10 +39,10 @@ export  const products = [
       title: '+ Cloves CG3',
       desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
     },
-    {
-      id: 6,
-      image: img5,
-      title: '+ Cloves CG3',
-      desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
-    },
+    // {
+    //   id: 6,
+    //   image: img5,
+    //   title: '+ Cloves CG3',
+    //   desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+    // },
   ];
